5b858eda098e0e4f59c0fa2c98c611f288974e69
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-19 04:12:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime_2?(number)
+  if number <= 1 
+    return false
+  end
+  idx= 2
+  while idx < number
+      remainder = number%idx
+      #puts "prime 1| number:#{number} divisor:#{idx}  remainder#{remainder}"
+      if remainder == 0 || number == 2
+          return false
+      end
+      idx += 1
+  end
+  return true
+end
